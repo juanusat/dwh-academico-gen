dd77e180e1016d13eb84779a6ff63bd5f0fed198
juanusat <71309030@usat.pe>
2025-05-11 17:10:52 -0500

    Select para cantidad de filas ppor tabla

--- notas/sql-consultas-progreso.sql
+++ notas/sql-consultas-progreso.sql
@@ -8,3 +8,13 @@ UNION ALL
 SELECT 'detalle_matricula', COUNT(*) FROM detalle_matricula
 UNION ALL
 SELECT 'curso_programado', COUNT(*) FROM curso_programado;
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--S02: Cantidad de filas en cada tabla
+SELECT
+    relname AS table_name,
+    n_live_tup AS row_count
+FROM
+    pg_stat_user_tables
+ORDER BY
+    row_count DESC;
